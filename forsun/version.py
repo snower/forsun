4a7e00bc56988a9ebc2216c8d0639c4ca60e55c0
snower <sujian199@gmail.com>
2018-02-08 10:45:46 +0800

    change version to 0.0.3

--- forsun/version.py
+++ forsun/version.py
@@ -2,5 +2,5 @@
 # 18/1/30
 # create by: snower
 
-version = "0.0.2"
-version_info = (0,0,2)
+version = "0.0.3"
+version_info = (0,0,3)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 import os
 from setuptools import find_packages, setup
 
-version = "0.0.2"
+version = "0.0.3"
 
 if os.path.exists("README.rst"):
     with open("README.rst") as fp:

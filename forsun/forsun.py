53e26a8d46fa5491e1a801f4f014483bae26569d
sunlong <sunlong@ejoyby.com>
2018-08-31 15:50:21 +0800

    fix forsun init check

--- forsun/forsun.py
+++ forsun/forsun.py
@@ -188,13 +188,20 @@ class Forsun(object):
                 self.current_time = yield self.store.get_current()
                 logging.info("start by last time %s current time %s", self.current_time, ts)
 
-                while self.current_time > 0 and self.current_time < ts:
-                    self.ioloop.add_callback(self.handler, self.current_time)
-                    self.current_time += 1
+                @gen.coroutine
+                def check_last(self, current_time, ts):
+                    count = ts - current_time
+                    while current_time < ts:
+                        yield self.handler(current_time)
+                        current_time += 1
+                    logging.info("end by last time %s current time %s", current_time - count, ts)
+
+                if self.current_time > 0:
+                    self.ioloop.add_callback(check_last, self, self.current_time, ts)
 
             self.current_time = ts
             yield self.store.set_current(self.current_time)
-            self.ioloop.add_callback(self.handler, ts)
+            yield self.handler(ts)
         except Exception as e:
             logging.error("check error: %s %s", ts, e)
 

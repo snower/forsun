f1dea5136566e6097bd411ff367a6726d2284236
sujian <sujian@aecg.com.cn>
2022-07-12 14:49:51 +0800

    optimization docker startup script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM python:3.6.15
 
 MAINTAINER snower sujian199@gmail.com
 
@@ -11,16 +11,12 @@ WORKDIR /forsun
 
 COPY docker/startup.sh /opt/
 
-RUN apt-get update \
-    && apt-get install -y python \
-    && apt-get install -y python-pip \
-    && pip install tornado==5.1 \
+RUN pip install tornado==5.1 \
     && pip install thrift==0.11.0 \
     && pip install torthrift==0.2.4 \
     && pip install tornadis==0.8.1 \
     && pip install msgpack==0.5.1 \
     && pip install forsun==0.1.4 \
-    && pip install beanstalkt==0.7.0 \
     && pip install pymysql==0.7.10 \
     && pip install tormysql==0.3.7 \
     && chmod +x /opt/startup.sh
--- docker/startup.sh
+++ docker/startup.sh
@@ -10,16 +10,16 @@ fi
 
 cd /forsun || exit
 
-CMD_ARG="--bind=\"0.0.0.0\" --http=\"0.0.0.0:9002\" --port=6458 --nodemon"
+CMD_ARG="--bind=0.0.0.0 --http=0.0.0.0:9002 --port=6458 --nodemon"
 
 if [ -n "$ARG_LOG" ]; then
-  CMD_ARG="$CMD_ARG --log=\"$ARG_LOG\""
+  CMD_ARG="$CMD_ARG --log=$ARG_LOG"
 else
   CMD_ARG="$CMD_ARG --log=-"
 fi
 
 if [ -n "$ARG_LOG_LEVEL" ]; then
-  CMD_ARG="$CMD_ARG --log_level=\"$ARG_LOG_LEVEL\""
+  CMD_ARG="$CMD_ARG --log_level=$ARG_LOG_LEVEL"
 fi
 
 DRIVER="mem"
@@ -32,7 +32,7 @@ if [ "$DRIVER" == "mem" ]; then
   if [ -n "$ARG_STORE_MEM_STORE_FILE" ]; then
     CMD_ARG="$CMD_ARG --driver-mem-store-file=$ARG_STORE_MEM_STORE_FILE"
   else
-    CMD_ARG="$CMD_ARG --driver-mem-store-file=/forsun/data"
+    CMD_ARG="$CMD_ARG --driver-mem-store-file=/forsun/data/session.dump"
   fi
 else
   CMD_ARG="$CMD_ARG --driver=redis"
@@ -45,7 +45,7 @@ else
   fi
 
   if [ -n "$ARG_DRIVER_REDIS_PASSWORD" ]; then
-    CMD_ARG="$CMD_ARG --driver-redis-password=\"$ARG_DRIVER_REDIS_PASSWORD\""
+    CMD_ARG="$CMD_ARG --driver-redis-password=$ARG_DRIVER_REDIS_PASSWORD"
   fi
 
   if [ -n "$ARG_DRIVER_REDIS_DB" ]; then
